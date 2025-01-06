1a35d7a27252e54d51d183803b9091d5c754858e
TassioNS9 <tassions@hotmail.com>
2025-01-06 18:26:18 -0300

    update bio agora

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className='footer'>
             <div className="footer-text">
-                <p>Copyright &copy; 2024 por Tassio Neves | Todos os Direitos reservados</p>
+                <p>Copyright &copy; 2025 por Tassio Neves | Todos os Direitos reservados</p>
             </div>
             <div className="footer-iconTop">
             <a href="#home"><i className='bx bx-up-arrow-alt'></i></a>
