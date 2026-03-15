f9d3f3325e2f6e718a3dfff5d0344c00e7af01f5
Tassio Neves <tassions@hotmail.com>
2026-03-15 15:56:58 -0300

    feat: update footer color

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,18 @@
-import './Footer.css'
+import "./Footer.css";
 export default function Footer() {
-
-    return (
-        <footer className='footer'>
-            <div className="footer-text">
-                <p>Copyright &copy; 2025 por Tassio Neves | Todos os Direitos reservados</p>
-            </div>
-            <div className="footer-iconTop">
-            <a href="#home"><i className='bx bx-up-arrow-alt'></i></a>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="footer">
+      <div className="footer-text">
+        <p>
+          Copyright &copy; {new Date().getFullYear()} por Tassio Neves | Todos
+          os Direitos reservados
+        </p>
+      </div>
+      <div className="footer-iconTop">
+        <a href="#home">
+          <i className="bx bx-up-arrow-alt"></i>
+        </a>
+      </div>
+    </footer>
+  );
 }
