706e20849e9fda3e50688b5fdf38644413a80011
Tassio Neves <tassions@hotmail.com>
2026-03-26 13:50:13 -0300

    feat: add news stacks to list

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,116 +1,84 @@
 import "./Services.css";
 export default function Services() {
+  const services = [
+    {
+      title: "Node",
+      description: `Node.js é um software de código aberto, multiplataforma.A principal
+            característica do Node.js é sua arquitetura assíncrona e orientada
+            por eventos`,
+      svgUrl: "nodejs-logo-svgrepo-com.svg",
+    },
+    {
+      title: "JavaScript",
+      description: `JavaScript é uma linguagem de programação interpretada estruturada,
+            de script em alto nível com tipagem dinâmica fraca e multiparadigma.`,
+      svgUrl: "javascript-svgrepo-com.svg",
+    },
+    {
+      title: "TypeScript",
+      description: `TypeScript é uma linguagem de programação fortemente tipada que se
+            baseia em JavaScript, oferecendo melhores ferramentas em qualquer
+            escala.`,
+      svgUrl: "typescript-svgrepo-com.svg",
+    },
+    {
+      title: "ReactJs",
+      description: `React é uma biblioteca front-end JavaScript de código aberto com
+            foco em criar interfaces de usuário em páginas web.`,
+      svgUrl: "react-svgrepo-com.svg",
+    },
+    {
+      title: "NextJs",
+      description: `Next.js é um framework React para construir aplicações web
+            full-stack. Você usa componentes React para criar interfaces de
+            usuário e Next.js para recursos adicionais e otimizações.`,
+      svgUrl: "nextjs-svgrepo-com.svg",
+    },
+    {
+      title: "VueJs",
+      description: `Vue.js é um framework JavaScript de código-aberto, focado no
+            desenvolvimento de interfaces de usuário e aplicativos de página
+            única.`,
+      svgUrl: "vue-svgrepo-com.svg",
+    },
+    {
+      title: "Python",
+      description:
+        "Python é uma linguagem de programação de alto nível, de propósito geral, conhecida por sua sintaxe simples e legível, sendo usada para desenvolvimento web, ciência de dados, inteligência artificial, automação, desenvolvimento de software e muito mais",
+      svgUrl: "python-svgrepo-com.svg",
+    },
+    {
+      title: "IA",
+      description:
+        " incorporação de modelos inteligentes em sistemas, softwares (ERP, CRM) e fluxos de trabalho existentes para automatizar tarefas, aumentando a eficiência operacional e reduzindo custos",
+      svgUrl: "ai-svgrepo-com.svg",
+    },
+    {
+      title: "Docker",
+      description:
+        "Docker é uma plataforma de código aberto que usa contêineres para empacotar, distribuir e executar aplicações, incluindo código, bibliotecas e dependências, garantindo que funcionem de forma consistente em qualquer ambiente.",
+      svgUrl: "docker-svgrepo-com.svg",
+    },
+    {
+      title: "Figma",
+      description: `Figma é um editor gráfico de vetor e prototipagem de projetos de
+            design baseado principalmente no navegador web.`,
+      svgUrl: "figma-svgrepo-com.svg",
+    },
+  ];
   return (
     <section className="services" id="services">
       <h2 className="heading">
         Nossos <span>Serviços</span>
       </h2>
       <div className="services-container">
-        <div className="services-box">
-          <i className="bx bx-code-alt"></i>
-          <h3>Desenvolvedor Web</h3>
-          <p>
-            A função de um desenvolvedor web é construir e manter
-            sites.Responsavéis por construir um produto que encontra ambas as
-            necessidades do cliente e aquelas do usuário final.
-          </p>
-          <a
-            href="https://harve.com.br/blog/desenvolvimento-web/o-que-faz-um-desenvolvedor-web-e-como-se-tornar-um-em-2023/#:~:text=A%20fun%C3%A7%C3%A3o%20de%20um%20desenvolvedor%20web%20%C3%A9%20construir%20e%20manter%20sites."
-            className="btn"
-          >
-            Leia Mais
-          </a>
-        </div>
-        <div className="services-box">
-          <i className="bx bxl-javascript"></i>
-          <h3>JavaScript</h3>
-          <p>
-            JavaScript é uma linguagem de programação interpretada estruturada,
-            de script em alto nível com tipagem dinâmica fraca e multiparadigma.
-          </p>
-          <a
-            href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript"
-            className="btn"
-          >
-            Leia Mais
-          </a>
-        </div>
-
-        <div className="services-box">
-          <i className="bx bxl-typescript"></i>
-          <h3>TypeScript</h3>
-          <p>
-            TypeScript é uma linguagem de programação fortemente tipada que se
-            baseia em JavaScript, oferecendo melhores ferramentas em qualquer
-            escala.
-          </p>
-          <a href="https://www.typescriptlang.org/" className="btn">
-            Leia Mais
-          </a>
-        </div>
-
-        <div className="services-box">
-          <i className="bx bxl-react"></i>
-          <h3>ReactJS</h3>
-          <p>
-            React é uma biblioteca front-end JavaScript de código aberto com
-            foco em criar interfaces de usuário em páginas web.
-          </p>
-          <a href="https://react.dev/learn" className="btn">
-            Leia Mais
-          </a>
-        </div>
-
-        <div className="services-box">
-          <i className="bx bxl-react"></i>
-          <h3>NextJS</h3>
-          <p>
-            Next.js é um framework React para construir aplicações web
-            full-stack. Você usa componentes React para criar interfaces de
-            usuário e Next.js para recursos adicionais e otimizações.
-          </p>
-          <a href="https://react.dev/learn" className="btn">
-            Leia Mais
-          </a>
-        </div>
-
-        <div className="services-box">
-          <i className="bx bxl-nodejs"></i>
-          <h3>NodeJS</h3>
-          <p>
-            Node.js é um software de código aberto, multiplataforma.A principal
-            característica do Node.js é sua arquitetura assíncrona e orientada
-            por eventos
-          </p>
-          <a href="https://nodejs.org/en/about" className="btn">
-            Leia Mais
-          </a>
-        </div>
-
-        <div className="services-box">
-          <i className="bx bxl-figma"></i>
-          <h3>Figma</h3>
-          <p>
-            Figma é um editor gráfico de vetor e prototipagem de projetos de
-            design baseado principalmente no navegador web.
-          </p>
-          <a href="https://www.alura.com.br/artigos/figma" className="btn">
-            Leia Mais
-          </a>
-        </div>
-
-        <div className="services-box">
-          <i className="bx bxl-vuejs"></i>
-          <h3>VueJS</h3>
-          <p>
-            Vue.js é um framework JavaScript de código-aberto, focado no
-            desenvolvimento de interfaces de usuário e aplicativos de página
-            única.
-          </p>
-          <a href="https://vuejs.org/" className="btn">
-            Leia Mais
-          </a>
-        </div>
+        {services.map((service) => (
+          <div key={service.title} className="services-box">
+            <img src={service.svgUrl} alt={service.title} width={45} />
+            <h3>{service.title}</h3>
+            <p>{service.description}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
