4dec9e880885623abfe2f9a784783456d2aed9a0
Tassio Neves <tassions@hotmail.com>
2026-03-26 13:48:18 -0300

    feat: add news  projects to list

--- src/components/Portifolio/Portifolio.jsx
+++ src/components/Portifolio/Portifolio.jsx
@@ -1,160 +1,85 @@
 import "./Portifolio.css";
-import web_store from "/public/assets/web-store-image.png";
-import logo_android from "/public/assets/logo-android.jpg";
-import finance_image from "/public/assets/finance_image.png";
-import ordem_livros from "/public/assets/ordem-livros.png";
-import tn_lib from "/public/assets/tn_lib.png";
-import tnt_flix from "/public/assets/tnt_flix.png";
-import logo_burguer from "/public/assets/logo_burger.png";
-import capputeeno from "/public/assets/capputeeno_image.png";
-import usf_image from "/public/assets/usf_image.png";
+import web_store from "/assets/web-store-image.png";
+import finance_image from "/assets/finance_image.png";
+import ordem_livros from "/assets/ordem-livros.png";
+import tn_lib from "/assets/tn_lib.png";
+import usf_image from "/assets/usf_image.png";
+import mcdonalds from "/assets/mcdonalds.png";
+import trips from "/assets/trips.png";
+import aparatus from "/assets/aparatus.png";
 export default function Portifolio() {
+  const projects = [
+    {
+      title: "Finance AI",
+      description: "Projeto em NextJS de um sistema de finanças.",
+      imgUrl: finance_image,
+      link: "https://finance-ai-khaki.vercel.app/login",
+    },
+    {
+      title: "Projeto Loja TNS Imports",
+      description: "Projeto com uma loja web de acessórios de computadores!",
+      imgUrl: web_store,
+      link: "https://fsw-store-oxn5.vercel.app/",
+    },
+    {
+      title: "Ordem dos Livros",
+      description: "Projeto que demostra a ordem Cronológica dos livros",
+      imgUrl: ordem_livros,
+      link: "https://ordemlivrostn.netlify.app/series/harry-potter",
+    },
+    {
+      title: "Trips",
+      description:
+        "Uma plataforma de busca e reservas de viagens estilo AirBNB ",
+      imgUrl: trips,
+      link: "https://fsw-trips-tau.vercel.app/",
+    },
+    {
+      title: "Mcdonald's",
+      description:
+        "uma plataforma de pedidos de lanches replica do McDonald's ",
+      imgUrl: mcdonalds,
+      link: "https://mcdonalds-fecf.vercel.app/fsw-donalds",
+    },
+    {
+      title: "Filmes",
+      description: "Web de livraria de filmes completo consumindo api",
+      imgUrl: tn_lib,
+      link: "https://nevesfilmes-lib.vercel.app/",
+    },
+    {
+      title: "TN Barbeshop",
+      description: "Um SaaS de Barbearia com integração com chatBot!",
+      imgUrl: aparatus,
+      link: "https://github.com/tassioNS9/aparatus",
+    },
+
+    {
+      title: "USF Sistemas",
+      description: `Sistema web de indicadores de saúde para mulheres desenvolvido
+              como trabalho de TCC`,
+      imgUrl: usf_image,
+      link: "https://github.com/tassioNS9/usf-frontend",
+    },
+  ];
   return (
     <section className="portifolio" id="portifolio">
       <h2 className="heading">
         Ultimos <span>Projetos</span>
       </h2>
       <div className="portifolio-container">
-        <div className="portifolio-box">
-          <img src={web_store} alt="web-store" />
-          <div className="portifolio-layer">
-            <h4>Projeto Loja Web TNS Imports</h4>
-            <p>Projeto com uma loja web de acessórios de computadores!</p>
-            <a
-              href="https://fsw-store-oxn5.vercel.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bx bx-link-external"></i>
-            </a>
+        {projects.map((project) => (
+          <div key={project.title} className="portifolio-box">
+            <img src={project.imgUrl} alt="finance_image" />
+            <div className="portifolio-layer">
+              <h4>{project.title}</h4>
+              <p>{project.description}</p>
+              <a href={project.link} target="_blank" rel="noreferrer">
+                <i className="bx bx-link-external"></i>
+              </a>
+            </div>
           </div>
-        </div>
-
-        <div className="portifolio-box">
-          <img src={finance_image} alt="finance_image" />
-          <div className="portifolio-layer">
-            <h4>Finance AI</h4>
-            <p>Projeto em NextJS de um sistema de finanças</p>
-            <a
-              href="https://finance-ai-khaki.vercel.app/login"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bx bx-link-external"></i>
-            </a>
-          </div>
-        </div>
-
-        <div className="portifolio-box">
-          <img src={tnt_flix} alt="tnt_flix_image" />
-          <div className="portifolio-layer">
-            <h4>Filmes</h4>
-            <p>
-              Simples página home desenvolvida clone da netflix consumido api
-              apenas com javascript
-            </p>
-            <a
-              href="https://github.com/tassioNS9/netflix-clone"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bx bx-link-external"></i>
-            </a>
-          </div>
-        </div>
-
-        <div className="portifolio-box">
-          <img src={logo_burguer} alt="burger image" />
-          <div className="portifolio-layer">
-            <h4>Burger</h4>
-            <p>Projeto de um simples fastfood de hamburguer usando VueJs </p>
-            <a
-              href="https://github.com/tassioNS9/make_your_burguer"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bx bx-link-external"></i>
-            </a>
-          </div>
-        </div>
-
-        <div className="portifolio-box">
-          <img src={ordem_livros} alt="logo-android" />
-          <div className="portifolio-layer">
-            <h4>Ordem dos Livros</h4>
-            <p>Projeto que demostra a ordem Cronológica dos livros</p>
-            <a
-              href="https://ordemlivrostn.netlify.app/series/harry-potter"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bx bx-link-external"></i>
-            </a>
-          </div>
-        </div>
-
-        <div className="portifolio-box">
-          <img src={tn_lib} alt="logo-android" />
-          <div className="portifolio-layer">
-            <h4>Filmes</h4>
-            <p>Web de livraria de filmes completo consumindo api</p>
-            <a
-              href="https://nevesfilmes-lib.vercel.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bx bx-link-external"></i>
-            </a>
-          </div>
-        </div>
-
-        <div className="portifolio-box">
-          <img src={capputeeno} alt="capputeeno" />
-          <div className="portifolio-layer">
-            <h4>Capputeeno</h4>
-            <p>Projeto web de um eccommerce de canecas e camisas</p>
-            <a
-              href="https://github.com/tassioNS9/ecommerce-capputeeno"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bx bx-link-external"></i>
-            </a>
-          </div>
-        </div>
-
-        <div className="portifolio-box">
-          <img src={logo_android} alt="logo2" />
-          <div className="portifolio-layer">
-            <h4>Curiosidades</h4>
-            <p>Site desenvolvido falando um pouco sobre o Android</p>
-            <a
-              href="https://tassions9.github.io/site-android/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bx bx-link-external"></i>
-            </a>
-          </div>
-        </div>
-
-        <div className="portifolio-box">
-          <img src={usf_image} alt="usf_image" />
-          <div className="portifolio-layer">
-            <h4>USF Sistemas</h4>
-            <p>
-              Sistema web de indicadores de saúde para mulheres desenvolvido
-              como trabalho de TCC{" "}
-            </p>
-            <a
-              href="https://github.com/tassioNS9/usf-frontend"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="bx bx-link-external"></i>
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
