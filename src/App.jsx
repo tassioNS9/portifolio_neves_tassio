7c0f0debb15e49f1ac9d7eb078262107842f7b0c
Tassio Neves <tassions@hotmail.com>
2026-03-26 13:51:46 -0300

    feat: add new dependencies and config new colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-
-
-import './App.css'
-import About from './components/About/About'
-import Contact from './components/Contact/Contact'
-import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header'
-import Home from './components/Home/Home'
-import Portifolio from './components/Portifolio/Portifolio'
-import Services from './components/Services/Services'
-import ToastProvider from './providers/toast'
+import "./App.css";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Portifolio from "./components/Portifolio/Portifolio";
+import Services from "./components/Services/Services";
+import ToastProvider from "./providers/toast";
 
 function App() {
-
-
   return (
     <>
       <ToastProvider>
@@ -21,11 +17,11 @@ function App() {
         <About />
         <Services />
         <Portifolio />
-        {/* <Contact /> */}
+        <Contact />
         <Footer />
       </ToastProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
